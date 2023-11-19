fe1a64442a8b460454d37722c3c00f594c4b52e6
John Nasimba <nasimba4john@gmail.com>
2023-11-19 17:26:52 +0200

    modeal serializer

--- store/serializers.py
+++ store/serializers.py
@@ -2,12 +2,18 @@ from decimal import Decimal
 from rest_framework import serializers
 from store.models import Product, Collection
 
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    unit_price = serializers.DecimalField(max_digits=6, decimal_places=2)
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ['id', 'title']
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'unit_price', 'price_with_tax', 'collection']
+
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
-    collection = serializers.PrimaryKeyRelatedField(queryset=Collection.objects.all())
+
 
     def calculate_tax(self, product):
         return product.unit_price * Decimal(1.1)
